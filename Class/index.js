82dbc56690dac7ee362bca3bb7c0cc2a04d0ddbf
yanglei <yanglei@shjinjia.com>
2019-11-12 13:52:48 +0800

    class

--- Class/index.js
+++ Class/index.js
@@ -1,4 +1,23 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
 /**
  * 类
  */
@@ -81,5 +100,140 @@ var Animal = /** @class */ (function () {
 }());
 var dog = new Animal('欢欢');
 dog.changeName('豆豆');
-module.exports = {};
 // 5.继承
+/**
+ * 子类继承父类后子类的实例就拥有了父类中的属性和方法，可以增强代码的可复用性
+ * 将子类公用的方法抽象出来放在父类中，自己的特殊逻辑放在子类中重写父类的逻辑
+ * super可以调用父类上的方法和属性
+ */
+var People = /** @class */ (function () {
+    function People(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    People.prototype.getName = function () {
+        return this.name;
+    };
+    People.prototype.setName = function (name) {
+        this.name = name;
+    };
+    return People;
+}());
+var Student = /** @class */ (function (_super) {
+    __extends(Student, _super);
+    function Student(name, age, no) {
+        var _this = _super.call(this, name, age) || this;
+        _this.no = no;
+        return _this;
+    }
+    Student.prototype.getNo = function () {
+        return this.no;
+    };
+    return Student;
+}(People));
+var s1 = new Student('杨', 18, 1);
+console.log(s1);
+// 6.类里面的修饰符
+var Father = /** @class */ (function () {
+    function Father(name, age, money) {
+        this.name = name;
+        this.age = age;
+        this.money = money;
+    }
+    Father.prototype.getName = function () {
+        return this.name;
+    };
+    Father.prototype.setName = function (name) {
+        this.name = name;
+    };
+    return Father;
+}());
+var Child = /** @class */ (function (_super) {
+    __extends(Child, _super);
+    function Child(name, age, money) {
+        return _super.call(this, name, age, money) || this;
+    }
+    Child.prototype.desc = function () {
+        // console.log(`${this.name} ${this.age} ${this.money}`);  // 编译报错
+    };
+    return Child;
+}(Father));
+var child = new Child('杨', 10, 1000);
+console.log(child.name);
+// console.log(child.age); // 编译报错
+// console.log(child.money); // 编译报错
+// 7.静态属性 静态方法
+var Father1 = /** @class */ (function () {
+    function Father1(name) {
+        this.name = name;
+    }
+    Father1.getClassName = function () {
+        return Father1.className;
+    };
+    Father1.className = 'Father';
+    return Father1;
+}());
+console.log(Father1.className);
+console.log(Father1.getClassName());
+var child1 = /** @class */ (function (_super) {
+    __extends(child1, _super);
+    function child1(name) {
+        return _super.call(this, name) || this;
+    }
+    return child1;
+}(Father1));
+var c1 = new child1('11');
+// c1.getClassName()
+// 8.装饰器
+/**
+ * 装饰器是一种特殊类型的声明，它能够被附加到类声明、方法、属性或参数上，可以修改类的行为
+ * 常见的装饰器有类装饰器、属性装饰器、方法装饰器和参数装饰器
+ * 装饰器的写法分为普通装饰器和装饰器工厂
+ */
+//8.1.类装饰器
+/**
+ * 类装饰器在类声明之前声明，用来监视、修改或替换类定义
+ */
+var a;
+(function (a) {
+    function enhancer(target) {
+        target.prototype.name = 'yang';
+        target.prototype.eat = function () {
+            console.log('eat');
+        };
+    }
+    var Person = /** @class */ (function () {
+        function Person() {
+        }
+        Person = __decorate([
+            enhancer
+        ], Person);
+        return Person;
+    }());
+    var p = new Person();
+    console.log(p.name);
+    p.eat();
+})(a || (a = {}));
+var b;
+(function (b) {
+    function enhancer(name) {
+        return function enhancer(target) {
+            target.prototype.name = name;
+            target.prototype.eat = function () {
+                console.log('eat');
+            };
+        };
+    }
+    var Person = /** @class */ (function () {
+        function Person() {
+        }
+        Person = __decorate([
+            enhancer('yang')
+        ], Person);
+        return Person;
+    }());
+    var p = new Person();
+    console.log(p.name);
+    p.eat();
+})(b || (b = {}));
+module.exports = {};
--- Class/index.ts
+++ Class/index.ts
@@ -84,3 +84,145 @@ dog.changeName('豆豆')
 
 
 // 5.继承
+/**
+ * 子类继承父类后子类的实例就拥有了父类中的属性和方法，可以增强代码的可复用性
+ * 将子类公用的方法抽象出来放在父类中，自己的特殊逻辑放在子类中重写父类的逻辑
+ * super可以调用父类上的方法和属性
+ */
+
+class People {
+  name: string
+  age: number
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+  getName(): string {
+    return this.name
+  }
+  setName(name: string):void {
+    this.name = name;
+  }
+}
+
+class Student extends People {
+  no: number
+  constructor(name: string, age: number, no: number) {
+    super(name, age);
+    this.no = no;
+  }
+  getNo() {
+    return this.no;
+  }
+}
+let s1 = new Student('杨', 18, 1)
+console.log(s1);
+
+
+// 6.类里面的修饰符
+class Father {
+  public name: string //类里面 子类 其它任何地方外边都可以访问
+  protected age: number //类里面 子类 都可以访问,其它任何地方不能访问
+  private money: number //类里面可以访问， 子类和其它任何地方都不可以访问
+  constructor(name: string, age: number, money: number) {
+    this.name = name;
+    this.age = age;
+    this.money = money;
+  }
+  getName(): string{
+    return this.name;
+  }
+  setName(name: string): void{
+    this.name = name;
+  }
+}
+
+class Child extends Father {
+  constructor(name: string,age: number,money: number) {
+    super(name, age, money)
+  }
+  desc() {
+    // console.log(`${this.name} ${this.age} ${this.money}`);  // 编译报错
+  }
+}
+let child = new Child('杨',10,1000);
+console.log(child.name);
+// console.log(child.age); // 编译报错
+// console.log(child.money); // 编译报错
+
+
+// 7.静态属性 静态方法
+class Father1 {
+  static className='Father';
+  static getClassName() {
+    return Father1.className;
+  }
+  public name: string;
+  constructor(name:string) { //构造函数
+    this.name=name;
+  }
+}
+console.log(Father1.className);
+console.log(Father1.getClassName());
+class child1 extends Father1 {
+  constructor(name:string) {
+    super(name)
+  }
+}
+let c1 = new child1('11')
+// c1.getClassName()
+
+// 8.装饰器
+/**
+ * 装饰器是一种特殊类型的声明，它能够被附加到类声明、方法、属性或参数上，可以修改类的行为
+ * 常见的装饰器有类装饰器、属性装饰器、方法装饰器和参数装饰器
+ * 装饰器的写法分为普通装饰器和装饰器工厂
+ */
+
+ //8.1.类装饰器
+ /**
+  * 类装饰器在类声明之前声明，用来监视、修改或替换类定义
+  */
+
+  namespace a {
+    interface Person {
+      name: string;
+      eat: any
+    }
+    function enhancer (target: any) {
+      target.prototype.name = 'yang';
+      target.prototype.eat = function () {
+        console.log('eat')
+      }
+    }
+    @enhancer
+    class Person {
+      constructor() {}
+    }
+    let p: Person = new Person();
+    console.log(p.name);
+    p.eat();
+  }
+
+  namespace b {
+    interface Person {
+        name: string;
+        eat: any
+    }
+    function enhancer(name: string) {
+        return function enhancer(target: any) {
+            target.prototype.name = name;
+            target.prototype.eat = function () {
+                console.log('eat');
+            }
+        }
+    }
+
+    @enhancer('yang')
+    class Person {
+        constructor() { }
+    }
+    let p: Person = new Person();
+    console.log(p.name);
+    p.eat();
+}
