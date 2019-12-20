12c097b81ff359c91a0fd077a263ee1a32c5be54
yanglei <yanglei@shjinjia.com>
2019-12-20 10:58:44 +0800

    泛型

--- Class/index.js
+++ Class/index.js
@@ -455,7 +455,7 @@ var Dog = /** @class */ (function (_super) {
 }(Animal));
 function double(val) {
     if (typeof val === "string") {
-        return "string->" + val;
+        return "string=>" + val;
     }
     else if (typeof val === "number") {
         return val + val;
@@ -463,4 +463,179 @@ function double(val) {
 }
 console.log(double(1));
 console.log(double('a'));
+// 10.3 继承 vs 多态
+/**
+ * 继承(Inheritance)子类继承父类，子类除了拥有父类的所有特性外，还有一些更具体的特性
+ * 多态(Polymorphism)由继承而产生了相关的不同的类，对同一个方法可以有不同的行为
+ */
+var Animal2 = /** @class */ (function () {
+    function Animal2() {
+    }
+    Animal2.prototype.speak = function (word) {
+        return 'Animal' + word;
+    };
+    return Animal2;
+}());
+var Dog1 = /** @class */ (function (_super) {
+    __extends(Dog1, _super);
+    function Dog1() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    Dog1.prototype.speak = function (word) {
+        return 'Dog' + word;
+    };
+    return Dog1;
+}(Animal2));
+var Cat1 = /** @class */ (function (_super) {
+    __extends(Cat1, _super);
+    function Cat1() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    Cat1.prototype.speak = function (word) {
+        return 'Cat' + word;
+    };
+    return Cat1;
+}(Animal2));
+var dog1 = new Dog1();
+var cat1 = new Cat1();
+dog1.speak('hello');
+cat1.speak('hello');
+var speaker = {
+    speak: function () { }
+};
+var r = {
+    width: 10,
+    height: 10
+};
+// 一个类要实现一个接口，必须实现接口中所有方法
+var Bird = /** @class */ (function () {
+    function Bird() {
+    }
+    Bird.prototype.eat = function () { };
+    Bird.prototype.move = function () { };
+    return Bird;
+}());
+var p1 = {
+    id: 1,
+    name: 'zhufeng',
+    age: 10,
+    gender: 0
+};
+var discount = function (price) {
+    return price * .8;
+};
+var arr = ["1", "2", "3"];
+console.log(arr);
+// 11.6 类接口 对类的约束
+// 11.7 构造函数的类型
+var Animal3 = /** @class */ (function () {
+    function Animal3(name) {
+        this.name = name;
+    }
+    return Animal3;
+}());
+function createAnimal(clazz, name) {
+    return new clazz(name);
+}
+var a1 = createAnimal(Animal3, 'zhufeng');
+console.log(a1.name);
+// 12 泛型(用的地方很多)
+/**
+ * 泛型（Generics）是指在定义函数、接口或类的时候，不预先指定具体的类型，而在使用的时候再指定类型的一种特性
+ * 泛型T作用域只限于函数内部使用
+ */
+// 12.1 泛型函数
+function createArray(length, value) {
+    var array = [];
+    for (var i = 0; i < length; i++) {
+        array[i] = value;
+    }
+    return array;
+}
+var arr1 = createArray(5, '1');
+console.log(arr1);
+// 12.2 类数组
+/**
+ * 类数组（Array-like Object）类似数组，不是数组类型，比如 arguments
+ */
+function sum() {
+    var args = arguments;
+    for (var i = 0; i < args.length; i++) {
+        console.log(args[i]);
+    }
+}
+// sum(1, 2, 3);
+var root = document.getElementById('root');
+var children = root.children;
+children.length;
+var nodeList = root.childNodes;
+nodeList.length;
+// 12.3 泛型类 用到了泛型的类
+var MyArray = /** @class */ (function () {
+    function MyArray() {
+        this.list = [];
+    }
+    MyArray.prototype.add = function (element) {
+        this.list.push(element);
+    };
+    MyArray.prototype.getMax = function () {
+        var maxValue = this.list[0];
+        for (var i = 0; i < this.list.length; i++) {
+            var element = this.list[i];
+            if (element > maxValue) {
+                maxValue = element;
+            }
+        }
+        return maxValue;
+    };
+    return MyArray;
+}());
+var arr2 = new MyArray();
+arr2.add(1);
+arr2.add(2);
+arr2.add(3);
+var ret = arr2.getMax();
+console.log(ret);
+var add = function (a, b) {
+    return a;
+};
+add(1, 2);
+// 12.5 多个类型参数
+/**
+ * 泛型可以有多个
+ */
+//  不借助中间变量，交换两个变量的值
+function swap(tuple) {
+    return [tuple[1], tuple[0]];
+}
+var ret1 = swap([1, "a"]); // 交换位置
+ret1[0].toLowerCase();
+ret1[1].toFixed(2);
+// 12.6 默认泛型类型
+// 如果不传类型的话，默认是number类型
+function createArray3(length, value) {
+    var result = [];
+    for (var i = 0; i < length; i++) {
+        result[i] = value;
+    }
+    return result;
+}
+var result = createArray3(3, 'a');
+console.log(result);
+//可以让泛型继承一个接口
+function logger2(val) {
+    console.log(val.length);
+}
+logger2("yang");
+var cart = {
+    list: [1, 2, 3]
+};
+var cart1 = { list: ['1'] };
+var cart2 = [1];
 module.exports = {};
+// 12.10 泛型接口 vs 泛型类型别名
+/**
+ * 接口创建了一个新的名字，它可以在其他任意地方被调用。而类型别名并不创建新的名字，例如报错信息就不会使用别名
+ * 类型别名不能被 extends和 implements,这时我们应该尽量使用接口代替类型别名
+ * 当我们需要使用联合类型或者元组类型的时候，类型别名会更合适
+ */
